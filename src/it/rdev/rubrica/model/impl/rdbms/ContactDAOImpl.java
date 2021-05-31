dd64a1e073807cb75c6343171388aad413428598
Valix93 <v.lipari93@gmail.com>
2021-05-31 17:41:28 +0200

    Update ContactDAOImpl.java

--- src/it/rdev/rubrica/model/impl/rdbms/ContactDAOImpl.java
+++ src/it/rdev/rubrica/model/impl/rdbms/ContactDAOImpl.java
@@ -1,4 +1,4 @@
-package it.rdev.rubrica.model.impl.rdbms;
+package it.rdev.rubrica.model.impl;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,9 +8,9 @@ import java.util.List;
 import it.rdev.rubrica.model.Contact;
 import it.rdev.rubrica.model.ContactDAO;
 
-public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
+public class ContactDAOImpl extends AbstractDAO implements ContactDAO {
 	
-	private final String TABLE_NAME = "contacts";
+	private final String TABLE_NAME = "contatti";
 
 	public List<Contact> getAll() {
 		List<Contact> contacts = new ArrayList<>();
@@ -18,30 +18,16 @@ public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
 			ResultSet rs = this.executeQuery("SELECT id, name, surname FROM " + TABLE_NAME);
 			while(rs.next()) {
 				contacts.add(
-						new Contact()
-						.setId(rs.getInt("id"))
-						.setName(rs.getString("name"))
-						.setSurname(rs.getString("surname")));
+							new Contact()
+								.setId(rs.getInt("id"))
+								.setName(rs.getString("name"))
+								.setSurname(rs.getString("surname"))
+						);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return contacts;
-	}
-
-	@Override
-	public boolean persist(Contact o) throws SQLException {
-		return false;
-	}
-
-	@Override
-	public boolean delete(Contact t) throws SQLException {
-		return false;
-	}
-
-	@Override
-	public boolean update(Contact t) throws SQLException {
-		return false;
 	}
 
 }
